b61abe1a50868f3bad51d2730a8ef06a83b5a45b
SirshaGayen <62745779+SirshaGayen@users.noreply.github.com>
2020-07-30 01:49:56 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,9 @@
-insert into PAPER values (4,'ABP');
-insert into PAPER values(5,'TOI');
+--insert into PAPER values (4,'ABP');
+--insert into PAPER values(5,'TOI');
+
+insert into ITEMS values (1,'Item1',100.50);
+insert into ITEMS values(2,'Item2',200.08);
+
+
+insert into buyers(id,buyer_Name,address,gstin,state_Code) values(1,'Dey Bros', 'Db adrress, burabazar', '1567abcd',20);
+insert into buyers(id,buyer_Name,address,gstin,state_Code) values(2,'Anant Hardware', '113 B, Monohar Das Chowk, kol-7', '10067abcd',19);
