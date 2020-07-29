745153d3b2080abb2534ecd02a8ed55f6cde3dcb
SirshaGayen <62745779+SirshaGayen@users.noreply.github.com>
2020-07-30 01:41:24 +0530

    Update PaperService.java

--- src/main/java/infy1/PaperService.java
+++ src/main/java/infy1/PaperService.java
@@ -2,9 +2,18 @@ package infy1;
 
 import java.util.List;
 
+import infy2.BuyerModel;
+import infy2.ItemModel;
+import infy2.OrderModel;
 import infy2.Paper12;
 
 public interface PaperService {
 
 	public List<Paper12> getAllPapers();
+	
+	public List<ItemModel> getAllItems();
+	
+	public String saveOrder( OrderModel orderModel);
+	
+	public List<BuyerModel> fetchBuyerInfo();
 }
